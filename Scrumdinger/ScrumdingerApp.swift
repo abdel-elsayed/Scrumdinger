120c65d4a7b0fd6933e94d258b1b9d418331b0b8
Elsayed, Abdelrahman <abdelrahman.elsayed@lowes.com>
2024-07-20 22:26:59 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Elsayed, Abdelrahman on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
